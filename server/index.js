51784aa35dc62d761786657de6191b949c73c904
“JHuang19960705 <abc850705@gmail.com>
2026-03-23 21:41:40 +0800

    漏洞修復報告：CORS 全開放設定

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ mongoose
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({ origin: process.env.CLIENT_ORIGIN || "http://localhost:3000" }));
 
 app.use("/api/user", authRoute);
 //要進去必須先登入
